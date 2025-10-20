00355880255a29a58ef6fdddbc6d993cc7a1be0d
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-10-20 12:33:14 +0900

    init first commit

---
+++ src/main/java/com/climbingCRM/climbingcrm/ClimbingcrmApplication.java
@@ -0,0 +1,13 @@
+package com.climbingCRM.climbingcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingcrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/climbingCRM/climbingcrm/ClimbingcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climbingCRM.climbingcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
